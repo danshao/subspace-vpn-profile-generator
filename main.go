b55e2bfd8d6a88c12dcac63d84f75e73fabb04a4
Hayden Wang <haydenwang@ecoworkinc.com>
2017-03-31 14:10:54 +0800

    modifying import path

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-  "./vpnprofile"
-  "fmt"
+	"fmt"
+	"gitlab.ecoworkinc.com/Subspace/vpn-profile-generator/vpnprofile"
 )
 
 func main() {
-  server := vpnprofile.Server{"my.vpn.host", "ecowork"}
-  user := vpnprofile.User{"carter", "1234"}
+	server := vpnprofile.Server{"my.vpn.host", "ecowork"}
+	user := vpnprofile.User{"carter", "1234"}
 
-  windowsProfile := server.GenerateProfile(vpnprofile.WINDOWS, user)
-  fmt.Println(windowsProfile)
+	windowsProfile := server.GenerateProfile(vpnprofile.WINDOWS, user)
+	fmt.Println(windowsProfile)
 
-  appleProfile := server.GenerateProfile(vpnprofile.APPLE, user)
-  fmt.Println(appleProfile)
+	appleProfile := server.GenerateProfile(vpnprofile.APPLE, user)
+	fmt.Println(appleProfile)
 }
